9c65b82e34d9c7cc6b54ff676b660e63b2ed07af
techuy_qa <44953955+techuy06@users.noreply.github.com>
2025-01-22 13:18:45 +0700

    Create testphp.php

---
+++ testphp.php
@@ -0,0 +1,15 @@
+<?php
+// List all PHP files in the current directory
+$files = glob("*.php");
+
+if (!empty($files)) {
+    echo "PHP files in the current directory:<br><br>";
+    foreach ($files as $file) {
+        echo "<strong>File:</strong> " . htmlspecialchars($file) . "<br>";
+        echo "<strong>Code:</strong><br>";
+        echo "<pre>" . htmlspecialchars(file_get_contents($file)) . "</pre><br>";
+    }
+} else {
+    echo "No PHP files found in the current directory.";
+}
+?>
